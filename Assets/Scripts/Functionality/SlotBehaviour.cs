dc98c082b3dd5e8190c2134aef6deb5824d6073c
Sayan <sayanguha.underpinservices@gmail.com>
2025-02-27 16:28:25 +0530

    low balance update

--- Assets/Scripts/Functionality/SlotBehaviour.cs
+++ Assets/Scripts/Functionality/SlotBehaviour.cs
@@ -302,7 +302,7 @@ public class SlotBehaviour : MonoBehaviour
         if (LineBet_text) LineBet_text.text = SocketManager.initialData.Bets[BetCounter].ToString();
         if (TotalBet_text) TotalBet_text.text = (SocketManager.initialData.Bets[BetCounter] * Lines).ToString();
         currentTotalBet = SocketManager.initialData.Bets[BetCounter] * Lines;
-        CompareBalance();
+        // CompareBalance();
     }
 
     private void ChangeBet(bool IncDec)
@@ -327,7 +327,7 @@ public class SlotBehaviour : MonoBehaviour
         if (LineBet_text) LineBet_text.text = SocketManager.initialData.Bets[BetCounter].ToString();
         if (TotalBet_text) TotalBet_text.text = (SocketManager.initialData.Bets[BetCounter] * Lines).ToString();
         currentTotalBet = SocketManager.initialData.Bets[BetCounter] * Lines;
-        CompareBalance();
+        // CompareBalance();
     }
 
     #region InitialFunctions
